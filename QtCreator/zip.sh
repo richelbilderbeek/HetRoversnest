48824b9749a59b3c2a79c0115e90313d901cb75a
richelbilderbeek <richel@richelbilderbeek.nl>
2015-07-26 21:56:46 +0200

    Improved zip script, put zip here

---
+++ QtCreator/zip.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir CityOfThieves
+
+# Go to folder /HetRoversnest/QtCreator/CityOfThieves
+cd CityOfThieves
+
+# Create folder /HetRoversnest/QtCreator/CityOfThieves/Exe
+mkdir Exes
+
+cp -r ../../Files .
+
+cp ../release/HetRoversnestConsole.exe ./Exes/CityOfThievesConsole.exe
+cp ../release/HetRoversnestDesktop.exe ./Exes/CityOfThievesDesktop.exe
+
+# Go to folder /HetRoversnest/QtCreator/CityOfThieves
+cd ..
+
+zip -9 -r CityOfThieves.zip CityOfThieves
+
+rm -rf CityOfThieves
+
+mv CityOfThieves.zip ../CityOfThieves.zip
