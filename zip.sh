c19617323baf2eafbd63685762a1b2e2d01c6293
richelbilderbeek <richel@richelbilderbeek.nl>
2015-08-06 11:59:18 +0200

    Scripts work fine

--- zip.sh
+++ zip.sh
@@ -2,7 +2,7 @@
 
 CONSOLE_EXE=GameCityOfThievesConsole.exe
 DESKTOP_EXE=GameCityOfThievesDesktop.exe
-CONSOLE_NDS=./Nds/GameCityOfThievesConsole.nds
+CONSOLE_NDS=./Nds/GameCityOfThieves.nds
 
 if [ ! -e $CONSOLE_EXE ]
 then
@@ -44,7 +44,7 @@ mkdir CityOfThieves
 cp LICENSE ./CityOfThieves/LICENSE
 cp $CONSOLE_EXE ./CityOfThieves/GameCityOfThievesConsole.exe
 cp $DESKTOP_EXE ./CityOfThieves/GameCityOfThievesDesktop.exe
-cp $CONSOLE_NDS ./CityOfThieves/GameCityOfThievesConsole.nds
+cp $CONSOLE_NDS ./CityOfThieves/GameCityOfThieves.nds
 
 zip -9 -r GameCityOfThieves.zip CityOfThieves
 
